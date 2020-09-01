c8731319ae7617c96a5bea1844d82bc3a6cf735c
MrAkashKumar <akash.ku9596@gmail.com>
2020-09-01 23:21:43 +0530

    security-impl

---
+++ src/main/java/com/akash/securityOAuth/SpringBootSecurityRestOAuthApplication.java
@@ -0,0 +1,13 @@
+package com.akash.securityOAuth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecurityRestOAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSecurityRestOAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/akash/securityOAuth/SpringBootSecurityRestOAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akash.securityOAuth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSecurityRestOAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
